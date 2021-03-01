90edd3294e69121ca13b5de4aac2335e1b14353e
S2LF <sdeuhelf@gmail.com>
2021-03-01 15:39:34 +0100

    init projet & add basic dnd elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import './App.css';
 
-function App(): JSX.Element {
+type ColumnType = {
+  children: JSX.Element | boolean;
+  className: string;
+  title: string;
+};
+
+type MovableItemType = {
+  setIsFirstColumn: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const MovableItem = ({ setIsFirstColumn }: MovableItemType) => {
+  const [{ isDragging }, drag] = useDrag({
+    item: { name: 'Any custom name', type: 'Our first type' },
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult();
+      if (dropResult && dropResult.name === 'Column 1') {
+        setIsFirstColumn(true);
+      } else {
+        setIsFirstColumn(false);
+      }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const opacity = isDragging ? 0.4 : 1;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={drag} className="movable-item" style={{ opacity }}>
+      We will move this item
+    </div>
+  );
+};
+
+const Column = ({ children, className, title }: ColumnType) => {
+  const [, drop] = useDrop({
+    accept: 'Our first type',
+    drop: () => ({ name: title }),
+  });
+
+  return (
+    <div ref={drop} className={className}>
+      {title}
+      {children}
+    </div>
+  );
+};
+
+export default function App(): JSX.Element {
+  const [isFirstColumn, setIsFirstColumn] = useState(true);
+
+  const Item = <MovableItem setIsFirstColumn={setIsFirstColumn} />;
+
+  return (
+    <div className="container">
+      <DndProvider backend={HTML5Backend}>
+        <Column title="Column 1" className="column first-column">
+          {isFirstColumn && Item}
+        </Column>
+        <Column title="Column 2" className="column second-column">
+          {!isFirstColumn && Item}
+        </Column>
+      </DndProvider>
     </div>
   );
 }
-
-export default App;
